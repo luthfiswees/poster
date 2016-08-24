06d62b4ee53e64fdb220bb627676cc0fe420fe5f
Luthfi <luthfiswees@gmail.com>
2016-08-25 04:34:14 +0700

    Fix bug in postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160823154632) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tracks", force: :cascade do |t|
     t.string   "artist"
     t.string   "track"
