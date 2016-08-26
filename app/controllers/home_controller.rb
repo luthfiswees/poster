6a296547bf137630d036b4bf57c80af143dc6fef
Luthfi <luthfiswees@gmail.com>
2016-08-26 23:49:14 +0700

    Add home controller and view

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def start_poster
+    render 'start_poster'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/start_poster'
+
+root :to => 'home#index'
+
   resources :tracks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
