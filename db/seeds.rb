fc936fc6186c26591231ddbc108acaa9aee00095
Luthfi <luthfiswees@gmail.com>
2016-08-27 12:11:12 +0700

    add seeds for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Track.create ([
+    {artist: 'Tulus', track: 'Ruang Sendiri', album: 'Ruang Sendiri' },
+    {artist: 'Isyana Sarasvati, Rayi Putra', track: 'Kau Adalah', album: 'EXPLORE!'},
+    {artist: 'Adhitia Sofyan', track: 'Blue Sky Collapse', album: 'Quiet Down'},
+    {artist: 'Chrisye', track: 'Cintaku', album: 'Everlasting Hits'},
+    {artist: 'Rizky Febian', track: 'Kesempurnaan Cinta', album: 'Kesempurnaan Cinta'},
+    {artist: 'Maliq & d\'Essentials', track: 'Setapak Sriwedari', album: 'Sriwedari'},
+    {artist: 'ONE OK ROCK', track: 'Last Dance', album: 'Last Dance'},
+    {artist: 'Toraboruta', track: 'kokoro', album: 'Torabotic Tunes'},
+    {artist: 'tokuP', track: 'SPiCa', album: 'SPiCa'},
+    {artist: 'Coldplay', track: 'Up&Up', album: 'A Head Full of Dreams'},
+    {artist: 'DNCE', track: 'Toothbrush', album: 'SWAAY'}
+])
